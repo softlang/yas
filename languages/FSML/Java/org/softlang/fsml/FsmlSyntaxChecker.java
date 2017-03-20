d8e76867f5f5b862f192af4aaf3f79ecd23c92a7
Ralf Laemmel <rlaemmel@gmail.com>
2017-03-20 19:29:01 +0100

    Releasing book privately

--- languages/FSML/Java/org/softlang/fsml/FsmlSyntaxChecker.java
+++ languages/FSML/Java/org/softlang/fsml/FsmlSyntaxChecker.java
@@ -6,13 +6,12 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import java.io.IOException;
 // END ...
 public class FsmlSyntaxChecker {
-	public static void main(String[] args)
-			throws IOException {
+	public static void main(String[] args) throws IOException {
 		FsmlParser parser =
 		  new FsmlParser(
 		    new CommonTokenStream(
 		      new FsmlLexer(
-		    	new ANTLRFileStream(args[0]))));
+		        new ANTLRFileStream(args[0]))));
 		parser.fsm();
 		System.exit(parser.getNumberOfSyntaxErrors()-Integer.parseInt(args[1]));
 	}
